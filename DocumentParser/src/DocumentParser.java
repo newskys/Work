81edc0af0c5c1a6357e012bc9f03c518d5eeb779
Vinckel <newskys@naver.com>
2014-05-18 12:11:06 +0900

    파일 R/W 구현 완료.

--- DocumentParser/src/DocumentParser.java
+++ DocumentParser/src/DocumentParser.java
@@ -1,8 +1,11 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -14,20 +17,23 @@ import org.jsoup.nodes.Document;
 /**
  * 
  * @author Vinckel
- * @version 20140518
+ * @version 20140519
  *
  */
 public class DocumentParser {
-	private static final String FILE_DIR = System.getenv("GOOGLE_DRIVE") + "\\취미\\OUDocs\\";
+	private static final String TABLE_NAME = "mid"; // 게시판 이름
+	private static final long SLEEP_INTERVAL = 500L;  // 파싱 딜레이
+	private static final String FILE_DIR = System.getenv("GOOGLE_DRIVE") + "\\취미\\OUDocs\\" + TABLE_NAME + "\\";
 	private static final Pattern txtPattern = Pattern.compile("^[0-9]+");
 	
-	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		int startNum = 0;
-		int endNum = 7730;
+		int endNum = 6795;
 
 		List<OUDoc> ouDocList = new ArrayList<OUDoc>();
 		File ouDocs[] = new File(FILE_DIR).listFiles();
+		BufferedReader br = null;
+		BufferedWriter bw = null;
 		
 		try {
 			for (int i = 0; i < ouDocs.length; i++) {
@@ -38,9 +44,9 @@ public class DocumentParser {
 						startNum = tmpNum;
 					}
 				}
-				File f = new File(FILE_DIR + ouDocs[i].getName());
-				char[] c = new char[(int) f.length()];
-				BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f),"UTF8"));
+				File readFile = new File(FILE_DIR + ouDocs[i].getName());
+				char[] c = new char[(int) readFile.length()];
+				br = new BufferedReader(new InputStreamReader(new FileInputStream(readFile), "UTF8"));
 				br.read(c);
 				
 				OUDoc tmpOUDoc = new OUDoc();
@@ -56,21 +62,31 @@ public class DocumentParser {
 		
 		System.out.println("docID " + (++startNum) + "부터 검색을 시작합니다.");
 		
-//		try {
-//			for (int docNum = startNum; docNum <= endNum; docNum++) {
-//				StringBuilder fileLoc = new StringBuilder(FILE_DIR).append("").append(docNum);
-//				
-//				File file = new File(fileLoc.toString());
-//				OUDoc tmpOUDoc = new OUDoc();
-//				Document doc = Jsoup.connect("http://todayhumor.com/?mid_" + docNum).get();
-//				tmpOUDoc.setRawHtml(doc.toString());
-//				tmpOUDoc.setTitle(doc.select(".view_subject").text());
-//				tmpOUDoc.setContent(doc.select(".view_content").text());
-//	
+		try {
+			for (int docNum = startNum; docNum <= endNum; docNum++) {
+				File writeFile = new File(FILE_DIR + docNum + ".txt");
+				OUDoc tmpOUDoc = new OUDoc();
+				Document doc = Jsoup.connect("http://todayhumor.com/?" + TABLE_NAME + "_" + docNum).get();
+				String title = doc.select(".view_subject").text();
+				
+				if (title == null || title.length() == 0) {
+					continue;
+				} else {
+					bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(writeFile), "UTF8"));
+					tmpOUDoc.setRawHtml(doc.toString());
+					tmpOUDoc.setTitle(title);
+					tmpOUDoc.setContent(doc.select(".view_content").text());
+					
+					bw.write(tmpOUDoc.getRawHtml());
+				}
+				
+				Thread.sleep(SLEEP_INTERVAL);
 //				System.out.println(tmpOUDoc.getRawHtml());
-//			}
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
